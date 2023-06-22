666e41a508b8af78c5eb6ab810f47e05cd46831b
Rhyswebdev <rhyswebdev@gmail.com>
2023-06-22 13:38:20 +0100

    Fix spacing on gallery

--- web/app/themes/dash-starter/resources/views/blocks/gallery.blade.php
+++ web/app/themes/dash-starter/resources/views/blocks/gallery.blade.php
@@ -14,7 +14,7 @@
   EnqueueScript: scripts/script.js
   EnqueueAssets: path/to/asset
 --}}
-<section class="container mx-auto my-3rem">
+<section class="container mx-auto my-[3rem]">
     <div id="photo-gallery" class="lg:grid lg:grid-cols-5 lg:gap-x-2 mt-5">
         @php
             $gallery = get_field('gallery');
